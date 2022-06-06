18df9e95e223f2815deec30fec165b346c156ed0
Spencer Longhurst <spencersean12@gmail.com>
2022-06-06 15:57:48 -0600

    added some broken styling

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import '../styling/About.css'
 
 function About() {
-  return 
+  return (
+    <div className='about'>
+      <div className='hello'>Hello, I am</div>
+      <div className='name'>
+        <div className='first_name'>
+          <div className='s'>S</div>
+          <div className='p'>p</div>
+          <div className='e_1'>e</div>
+          <div className='n_1'>n</div>
+          <div className='c'>c</div>
+          <div className='e_2'>e</div>
+          <div className='r'>r</div>
+        </div>
+        {/* <div className='last_name'>
+          <div className='l'>L</div>
+          <div className='o'>o</div>
+          <div className='2_n'>n</div>
+          <div className='s'>g</div>
+          <div className='s'>h</div>
+          <div className='s'>u</div>
+          <div className='s'>r</div>
+          <div className='s'>s</div>
+          <div className='s'>t</div>
+        </div> */}
+      </div>
+    </div>
+  )
 }
 
 export default About
