f2fdc7ad177b8167990d9aa4008f2d408ac71594
Spencer Longhurst <spencersean12@gmail.com>
2022-06-08 16:02:55 -0600

    added more styling and added resume link

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import '../styling/Header.css'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 function Header() {
   return (
     <div className='header'>
+      <div className='icons'>
+      <GitHubIcon className='icon'/>
+      <LinkedInIcon className='icon'/>
+      </div>
       <div className='header_options'>
         <a className='header_options_a active'>About</a>
         <a className='header_options_a'>Experience</a>
--- portfolio/src/components/Intro.js
+++ portfolio/src/components/Intro.js
@@ -3,33 +3,34 @@ import '../styling/Intro.css'
 
 function Intro() {
   return (
-    <div className='about'>
+    <div className='intro'>
       <div className='hello'>Hello, I am</div>
       <div className='name'>
         <div className='first_name'>
-          <div className='s'>S</div>
-          <div className='p'>p</div>
-          <div className='e_1'>e</div>
-          <div className='n_1'>n</div>
-          <div className='c'>c</div>
-          <div className='e_2'>e</div>
-          <div className='r'>r</div>
+          <p className='letter'>S</p>
+          <p className='letter'>p</p>
+          <p className='letter'>e</p>
+          <p className='letter'>n</p>
+          <p className='letter'>c</p>
+          <p className='letter'>e</p>
+          <p className='letter'>r</p>
         </div>
         <div className='last_name'>
-          <div className='l'>L</div>
-          <div className='o'>o</div>
-          <div className='n_2'>n</div>
-          <div className='g'>g</div>
-          <div className='h'>h</div>
-          <div className='u'>u</div>
-          <div className='r_2'>r</div>
-          <div className='s_2'>s</div>
-          <div className='t'>t</div>
+          <p className='letter'>L</p>
+          <p className='letter'>o</p>
+          <p className='letter'>n</p>
+          <p className='letter'>g</p>
+          <p className='letter'>h</p>
+          <p className='letter'>u</p>
+          <p className='letter'>r</p>
+          <p className='letter'>s</p>
+          <p className='letter'>t</p>
+          <p className='letter'>.</p>
         </div>
       </div>
-      <div className='info'>
-        <p>I am a frontend software engineer who specializes in React.js and CSS.</p>
-      </div>
+      <p className='subheading'>Frontend developer.</p>
+      <p className='info'>I specialize in React.js and CSS.</p>
+      <a className='resume' href='https://docs.google.com/document/d/1xyJEkF_-0z2FoL0JNzj4nq3SmgLd7NRyMPUJdYMxw_I/export?format=pdf'>Resume</a>
     </div>
   )
 }
