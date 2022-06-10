7bc2d1e40640ca117abb019ef99e24b9695a74ea
Spencer Longhurst <spencersean12@gmail.com>
2022-06-10 12:02:05 -0600

    finished styling for contact section

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import '../styling/Contact.css'
-import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 function Contact() {
   return (
     <div id='contact'>
       <h2 className='contact_header'>Contact Me</h2>
-      <p>I am currently looking for Front End React.js developer roles. I am always available for a chat on LinkedIn</p>
-      <p>Feel free to reach out if you would like to discuss any open roles you may know about!</p>
-      <div className='contact_icons'>
-        <GitHubIcon className='contact_icon'/>
-        <LinkedInIcon className='contact_icon'/>
+      <p className='contact_p_one'>I am currently looking for Front End React.js developer roles. I am always available for a chat on LinkedIn or an Email. Feel free to reach out if you would like to discuss any open roles you may know about!</p>
+      <div className='contact_cta'>
+        <a className='email' href='mailto:spencersean12@gmail.com'>spencersean12@gmail.com</a>
+        <LinkedInIcon className='contact_icon' onClick={() => window.open('https://www.linkedin.com/in/spencer-longhurst/', "_blank")}/>
       </div>
     </div>
   )
