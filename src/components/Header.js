38dc87497ef77b80833c18cdded672eb12ea2460
Spencer Longhurst <spencersean12@gmail.com>
2022-06-13 11:01:42 -0600

    fixed contact info centering

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import '../styling/Header.css'
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
